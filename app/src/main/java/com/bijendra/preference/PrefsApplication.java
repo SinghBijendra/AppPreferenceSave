d8448119fba4484ed89b3ef6b5dc5fdb60ff8d5d
SinghBijendra <rebijendra@gmail.com>
2016-12-03 18:14:26 +0530

    Initial commit

---
+++ app/src/main/java/com/bijendra/preference/PrefsApplication.java
@@ -0,0 +1,21 @@
+package com.bijendra.preference;
+
+
+import android.app.Application;
+import android.content.ContextWrapper;
+
+
+public class PrefsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize the Prefs class
+        new Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+    }
+}
